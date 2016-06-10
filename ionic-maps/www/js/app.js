2ddbb94f2472c4fc173e5b291a109011af8456eb
Maysa Marshallia <maysamarshallia@student.uns.ac.id>
2016-06-10 10:28:20 +0700

    hehehe v1.3

--- ionic-maps/www/js/app.js
+++ ionic-maps/www/js/app.js
@@ -1,4 +1,3 @@
-angular.module('starter', ['ionic'])
 angular.module('ionic-maps', ['ionic'])
 
 .run(function($ionicPlatform) {
@@ -16,26 +15,143 @@ angular.module('ionic-maps', ['ionic'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
-  });
+  })
 })
 
-.config(function($stateProvider, $urlRouterProvider){
+.controller('backCtrl',function($scope,$ionicHistory){
+  $scope.mygoback=function(){
+    $ionicHistory.goback();
+  };
+})
+
+.controller('facultyCtrl',function($scope, facultyService){
+$scope.fakultas=facultyService.getfakultas(); 
+
+})
+
+.controller('facultyDetailsCtr',function($scope,$stateParams, facultyService){
+$scope.fakultas=facultyService.getfacultass($stateParams.idfakultas);
+})
+
+.service('facultyService',function(){
+  return{
+    fakultas:[
+      {
+        id: "1",
+        nama:"MIPA",
+        informasi:"fakultas mipa berdiri sejak ggggggggggggg gasdufaer aweao ra oriav oi  eri erirfjjjjj fujfha . fauier au irv aorifuaworfh . ajofia oaie ifjaoiruaoweiruao rjoaewrawf, dfjbfaoub oeriana fieoafnoanoafi."
+      },
+      {
+        id: "2",
+        nama:"Teknik",
+        programstudi:"",
+        map:"",
+        informasi:"fakultas teknik berdiri sejak  ggggggggggggg gasdufaer aweao ra oriav oi  eri erirfjjjjj fujfha . fauier au irv aorifuaworfh . ajofia oaie ifjaoiruaoweiruao rjoaewrawf, dfjbfaoub oeriana fieoafnoanoafi."
+       
+      },
+      {
+        id: "3",
+        nama:"Kedokteran",
+        programstudi:"",
+        map:"",
+        informasi:"fakultas kedokteran berdiri sejak  ggggggggggggg gasdufaer aweao ra oriav oi  eri erirfjjjjj fujfha . fauier au irv aorifuaworfh . ajofia oaie ifjaoiruaoweiruao rjoaewrawf, dfjbfaoub oeriana fieoafnoanoafi."
+       
+      },
+       {
+        id: "4",
+        nama:"Pertanian",
+        programstudi:"",
+        map:"",
+        informasi:"fakultas pertanian berdiri sejak  ggggggggggggg gasdufaer aweao ra oriav oi  eri erirfjjjjj fujfha . fauier au irv aorifuaworfh . ajofia oaie ifjaoiruaoweiruao rjoaewrawf, dfjbfaoub oeriana fieoafnoanoafi."
+
+       
+      },
+      {
+        id: "5",
+        nama:"HUKUM",
+        programstudi:"",
+        map:"",
+        informasi:"fakultas HUKUM berdiri sejak  ggggggggggggg gasdufaer aweao ra oriav oi  eri erirfjjjjj fujfha . fauier au irv aorifuaworfh . ajofia oaie ifjaoiruaoweiruao rjoaewrawf, dfjbfaoub oeriana fieoafnoanoafi."
+        
+      },
+      {
+        id: "6",
+        nama:"FISIP",
+        programstudi:"",
+        map:"",
+        informasi:"fakultas FISIP berdiri sejak  ggggggggggggg gasdufaer aweao ra oriav oi  eri erirfjjjjj fujfha . fauier au irv aorifuaworfh . ajofia oaie ifjaoiruaoweiruao rjoaewrawf, dfjbfaoub oeriana fieoafnoanoafi."
+        
+      },
+      {
+        id: "7",
+        nama:"EKONOMi",
+        programstudi:"",
+        map:"",
+        informasi:"fakultas ekonomi berdiri sejak  ggggggggggggg gasdufaer aweao ra oriav oi  eri erirfjjjjj fujfha . fauier au irv aorifuaworfh . ajofia oaie ifjaoiruaoweiruao rjoaewrawf, dfjbfaoub oeriana fieoafnoanoafi."
+        
+      },
+      {
+        id: "8",
+        nama:"FIB",
+        programstudi:"",
+        map:"",
+        informasi:"fakultas ilmu budaya berdiri sejak  ggggggggggggg gasdufaer aweao ra oriav oi  eri erirfjjjjj fujfha . fauier au irv aorifuaworfh . ajofia oaie ifjaoiruaoweiruao rjoaewrawf, dfjbfaoub oeriana fieoafnoanoafi."
+        
+      },
+      {
+        id: "9",
+        nama:"FSSR",
+        programstudi:"",
+        map:"",
+        informasi:"fakultas sastra dan seni rupa berdiri sejak  ggggggggggggg gasdufaer aweao ra oriav oi  eri erirfjjjjj fujfha . fauier au irv aorifuaworfh . ajofia oaie ifjaoiruaoweiruao rjoaewrawf, dfjbfaoub oeriana fieoafnoanoafi."
+
+       
+      },
+       {
+        id: "10",
+        nama:"FKIP",
+        programstudi:"",
+        map:"",
+        informasi:"fakultas keguruan dan ilmu pendidikan berdiri sejak  ggggggggggggg gasdufaer aweao ra oriav oi  eri erirfjjjjj fujfha . fauier au irv aorifuaworfh . ajofia oaie ifjaoiruaoweiruao rjoaewrawf, dfjbfaoub oeriana fieoafnoanoafi."
+
+       
+      },
+    ],
+    getfakultas:function(){
+      return this.fakultas;
+    },
+
+    getfacultass:function(idfakultas){
+      for(i=0;i<this.fakultas.length;i++){
+        if(this.fakultas[i].id==idfakultas){
+          return this.fakultas[i];
+        }
+      }
+    }
+  }
+})
+
+.config(function($stateProvider,$urlRouterProvider){
   $stateProvider
- 
-  .state('home', {
-    url: '/home',
-    templateUrl: 'templates/home.html'
+  .state('home',{
+    url:'/home',
+    templateUrl:'templates/home.html'
   })
- 
   .state('faculty', {
-    url: '/faculty',
-    templateUrl: 'templates/faculty.html'
+      url: '/faculty',
+      templateUrl: 'templates/faculty.html',
+      controller:'facultyCtrl'
+    })
+  .state('facultyDetails',{
+    url:'/faculty/:idfakultas',
+    templateUrl:'templates/facultyDetails.html',
+    controller:"facultyDetailsCtr"
   })
-  .state('calender', {
+    .state('calender', {
       url: '/calender',
       templateUrl: 'templates/calender.html'
     })
-  .state('contact', {
+    .state('contact', {
       url: '/contact',
       templateUrl: 'templates/contact.html'
     })
@@ -43,15 +159,15 @@ angular.module('ionic-maps', ['ionic'])
       url: '/library',
       templateUrl: 'templates/library.html'
     })
-      .state('maps', {
+    .state('maps', {
       url: '/maps',
       templateUrl: 'templates/maps.html'
     })
-      .state('news', {
+    .state('news', {
       url: '/news',
       templateUrl: 'templates/news.html'
     })
-  .state('siakad', {
+    .state('siakad', {
       url: '/siakad',
       templateUrl: 'templates/siakad.html'
     })
@@ -61,21 +177,75 @@ angular.module('ionic-maps', ['ionic'])
     })
       .state('tour', {
       url: '/tour',
-      templateUrl: 'templates/tour.html'
+      templateUrl: 'templates/tour.html',
+      controller:'IntroCtrl'
     })
   ;
+  $urlRouterProvider.otherwise('/home');
 })
 
-.controller("MainCtrl", function($scope, $ionicSlideBoxDelegate) {
+.controller('IntroCtrl', function($scope, $state, $ionicSlideBoxDelegate) {
+ 
+ $scope.tour=[
+  {
+    foto:"img/images (5).jpg",
+    nama:"",
+    informasi:""
+  },
+  {
+    foto:"img/images (6).jpg",
+    nama:"",
+    informasi:""
+  },
+  {
+    foto:"img/images (7).jpg",
+    nama:"",
+    informasi:""
+  },
+  {
+    foto:"img/images (8).jpg",
+    nama:"",
+    informasi:""
+  },
+  {
+    foto:"img/images (9).jpg",
+    nama:"",
+    informasi:""
+  },
+  {
+    foto:"img/images (10).jpg",
+    nama:"",
+    informasi:""
+  },
+  {
+    foto:"img/images (11).jpg",
+    nama:"",
+    informasi:""
+  },
+  {
+    foto:"img/images (13).jpg",
+    nama:"",
+    informasi:""
+  },
+  {
+    foto:"img/images (14).jpg",
+    nama:"Kampus Mesem",
+    informasi:"Kampus II UNS, kampus nya anak psikologi. biarpun terpisah UNS tetap di hati"
+  }
+  ];
 
-    $scope.images = [url:'img/book.png'];
-        
-         setTimeout(function() {
-                $ionicSlideBoxDelegate.slide(0);
-                $ionicSlideBoxDelegate.update();
-                $scope.$apply();
-            });
-       
-    
-    
+  $scope.startApp = function() {
+  $state.go('main');
+  };
+  $scope.next = function() {
+    $ionicSlideBoxDelegate.next();
+  };
+  $scope.previous = function() {
+    $ionicSlideBoxDelegate.previous();
+  };
+$scope.slideChanged = function(index) {
+    $scope.slideIndex = index;
+  };
+  
+  
 })
